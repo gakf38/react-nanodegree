34d4eba762b5b3e7bde74ef52866d8e2009209a2
Garrett Knox <garrettknox@garretts-mbp-2.attlocal.net>
2018-04-04 21:41:12 -0500

    Refactor ListContacts
    Refactor the ListContacts component to be a stateless functional
    component

--- practice-examples/contacts/src/ListContacts.js
+++ practice-examples/contacts/src/ListContacts.js
@@ -1,17 +1,37 @@
-import React, { Component } from 'react'
+//import React, { Component } from 'react'
+import React from 'react'
 
-class ListContacts extends Component {
+function ListContacts(props) {
+	return (
+		<ol className='contact-list'>
+			{props.contacts.map((contact) => (
+				<li key={contact.id} className='contact-list-item'>		
+					<div 
+						className='contact-avatar'
+						style={{
+							backgroundImage: `url(${contact.avatarURL})`
+						}}
+					>
+					</div>
+					<div className='contact-details'>
+						<p>{contact.name}</p>
+						<p>{contact.handle}</p>
+					</div>
+					<button className='contact-remove'>
+						Remove
+					</button>
+				</li>
+			))}
+		</ol>
+	);
+}
 
+/*class ListContacts extends Component {
 	render() {
-
 		return (
-
 			<ol className='contact-list'>
-
 				{this.props.contacts.map((contact) => (
-
-					<li key={contact.id} className='contact-list-item'>
-						
+					<li key={contact.id} className='contact-list-item'>		
 						<div 
 							className='contact-avatar'
 							style={{
@@ -19,24 +39,18 @@ class ListContacts extends Component {
 							}}
 						>
 						</div>
-
 						<div className='contact-details'>
 							<p>{contact.name}</p>
 							<p>{contact.handle}</p>
 						</div>
-
 						<button className='contact-remove'>
 							Remove
 						</button>
-
 					</li>
-
 				))}
-
 			</ol>
-
 		)
 	}
-}
+}*/
 
 export default ListContacts
