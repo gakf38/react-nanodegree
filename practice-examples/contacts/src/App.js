5b99a0c73bb8edfaf778c258be9de0edbf4c3808
Garrett Knox <garrettknox@Garretts-MacBook-Pro-2.local>
2018-04-08 13:27:38 -0500

    Add the CreateContact to the App
    Preliminarily defined the CreateContact component and added it to be
    render based on the app’s state

--- practice-examples/contacts/src/App.js
+++ practice-examples/contacts/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
 
 class App extends Component {
     
     state = {
-        contacts: []
+        contacts: [],
+        screen: 'create'
     }
 
     componentDidMount() {
@@ -30,10 +32,19 @@ class App extends Component {
     render() {
         return (
             <div>
-                <ListContacts
-                    contacts={this.state.contacts} 
-                    onDeleteContact={this.removeContact}
-                />
+                { this.state.screen === 'list' && 
+                    (
+                        <ListContacts
+                            contacts={this.state.contacts} 
+                            onDeleteContact={this.removeContact}
+                        />
+                    )
+                }
+                { this.state.screen === 'create' && 
+                    (
+                        <CreateContact />
+                    )
+                }
             </div>
         )
     }
