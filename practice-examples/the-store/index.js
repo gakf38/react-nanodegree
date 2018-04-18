c4b58826c5c0b575e42d44c9fff8add80b99deb3
Garrett Knox <garrettknox@garretts-mbp-2.attlocal.net>
2018-04-17 22:57:12 -0500

    Initial Store Creation
    Defined createStore function which handles the creation of a store. The
    store includes a state, a getState function which allows a way to get
    the state from the store, a subscribe function which allows to add
    listeners to the store to be run on state changes and a dispatch
    function which allows for running updates (actions) against the store’s
    state

---
+++ practice-examples/the-store/index.js
@@ -0,0 +1,69 @@
+// Library Code - Close to a library that would come from NPM package
+function createStore(reducer) {
+
+	/*
+		The store should have 4 parts:
+		1. the state
+		2. interaction to get the state
+		3. interaction to listen for changes to the state
+		4. interaction to update the state
+	*/
+
+	// Define the State of the Store
+	let state 
+
+	// Define a way to get the State of the Store
+	const getState = () => state
+
+	// Define a way to listen for changes to the state
+	let listeners = []
+	const subscribe = (listener) => {
+		// Add the listener to the listeners array
+		listeners.push(listener)
+		// Return a function that can be invoked to 'unsubscribe' the added listener
+		return () => {
+			listeners = listeners.filter((l) => l !== listener)
+		}
+	}
+
+	// Define a way to update the state of the store
+	const dispatch = (action) => {
+		state = reducer(state, action)
+		listeners.forEach((listener) => listener())
+	}
+
+
+	return {
+		getState,
+		subscribe,
+		dispatch
+	}
+}
+
+// App Code - Close to code that the user would write
+function todo (state = [], action) {
+	if(action.type === 'ADD_TODO') 
+	{
+		return state.concat([action.todo])
+	}
+
+	return state
+}
+
+// Create the Store
+const store = createStore(todo)
+
+// Subscribe a listener to the Store
+store.subscribe(() => {
+	console.log('The new state is: ', store.getState())
+})	
+
+// Dispatch an action to the Store
+store.dispatch({
+	type: 'ADD_TODO',
+	todo: {
+		id: 0,
+		name: 'Learn Redux',
+		complete: false
+	}
+})
