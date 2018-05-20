12c6bbfcf2f0ce506ec632ec8cd07a54f28aa258
Garrett Knox <garrettknox@Garretts-MacBook-Pro-2.local>
2018-05-20 16:37:54 -0500

    Create the Redux Store

--- practice-examples/chirper-app/src/index.js
+++ practice-examples/chirper-app/src/index.js
@@ -1,6 +1,17 @@
+// React imports
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+// Redux imports
+import { createStore } from 'redux'
+import { Provider } from 'react-redux' 
+
+// Custom imports
 import './index.css'
 import App from './components/App'
+import reducer from './reducers'
+
+// Create Redux Store
+const store = createStore(reducer)
 
 ReactDOM.render(<App />, document.getElementById('root'))
