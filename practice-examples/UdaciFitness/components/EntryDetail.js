25767566b8f72c30a617370ee94b841547c7fa29
Garrett Knox <garrettknox@Garretts-MacBook-Pro-2.local>
2018-07-22 17:08:26 -0500

    Added Reset Functionality to EntryDetails Component
    Added a TextButton component with onPress functionality that dispatched
    functions related to reseting a metrics entry in Redux

--- practice-examples/UdaciFitness/components/EntryDetail.js
+++ practice-examples/UdaciFitness/components/EntryDetail.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import { white } from '../utils/colors'
+import MetricCard from './MetricCard'
+import TextButton from './TextButton'
+import { addEntry } from '../actions'
+import { removeEntry } from '../utils/api'
+import { timeToString, getDailyReminderValue } from '../utils/helpers'
 
 class EntryDetail extends Component {
 
@@ -15,13 +22,61 @@ class EntryDetail extends Component {
 		}
 	}
 
+	reset = () => {
+		const { remove, goBack, entryId } = this.props
+
+		remove()
+		goBack()
+		removeEntry(entryId)
+	}
+
+	shouldComponentUpdate (nextProps) {
+		return nextProps.metrics !== null && !nextProps.metrics.today
+	}
+
 	render() {
+		const { metrics } = this.props
+
 		return (
-			<View>
-				<Text>Entry Detail - { this.props.navigation.state.params.entryId }</Text>
+			<View style={styles.container}>
+				<MetricCard metrics={metrics} />
+				<TextButton onPress={this.reset} style={{margin: 20}}>
+					RESET
+				</TextButton>
 			</View>
 		)
 	}
 }
 
-export default EntryDetail
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: white,
+		padding: 15
+	}
+})
+
+function mapStateToProps(state, { navigation }) {
+	const { entryId } = navigation.state.params
+
+	return {
+		entryId,
+		metrics: state[entryId]
+	}
+}
+
+function mapDispatchToProps(dispatch, { navigation }) {
+	const { entryId } = navigation.state.params
+
+	return {
+		remove: () => dispatch(addEntry({
+			[entryId]: timeToString() === entryId
+				? getDailyReminderValue()
+				: null
+		})),
+		goBack: () => navigation.goBack()
+	}
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail)
